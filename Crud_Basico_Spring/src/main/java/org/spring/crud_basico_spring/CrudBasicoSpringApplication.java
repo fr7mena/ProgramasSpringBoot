2e7036807b436b061264bdc73fe0645f99f48f22
fr7mena <fr7mena@gmail.com>
2025-01-29 10:45:39 +0100

    Añadimos una nueva carpeta para el proyecto del CRUD

---
+++ Crud_Basico_Spring/src/main/java/org/spring/crud_basico_spring/CrudBasicoSpringApplication.java
@@ -0,0 +1,13 @@
+package org.spring.crud_basico_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBasicoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudBasicoSpringApplication.class, args);
+    }
+
+}
