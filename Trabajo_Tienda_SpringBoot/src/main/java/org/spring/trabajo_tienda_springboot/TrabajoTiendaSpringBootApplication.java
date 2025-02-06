d5545ab0bc192941d99963121f54f1217cd0b80d
fr7mena <fr7mena@gmail.com>
2025-02-06 14:01:59 +0100

    meto una carpeta de proyecto

---
+++ Trabajo_Tienda_SpringBoot/src/main/java/org/spring/trabajo_tienda_springboot/TrabajoTiendaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.spring.trabajo_tienda_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoTiendaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabajoTiendaSpringBootApplication.class, args);
+    }
+
+}
---
+++ Trabajo_Tienda_SpringBoot/src/test/java/org/spring/trabajo_tienda_springboot/TrabajoTiendaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.trabajo_tienda_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoTiendaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
