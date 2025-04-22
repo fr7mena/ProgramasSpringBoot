7eb4a14c9c1cda9e6f67661c61d09a8b2092d114
fr7mena <fr7mena@gmail.com>
2025-04-22 12:29:41 +0200

    plantilla proyecto completada con su propio proyecto asociado. Es el de finance pero sirve para cualquier proyecto cambiando el properties y el nombre del proyecto en el org, evidentemente asociando a la base de datos correspondiente.

---
+++ Plantilla_Proyectos_SpringBoot/src/main/java/org/spring/finance_app_proyecto/FinanceAppApplication.java
@@ -0,0 +1,13 @@
+package org.spring.finance_app_proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceAppApplication.class, args);
+    }
+
+}
---
+++ Plantilla_Proyectos_SpringBoot/src/test/java/org/spring/finance_app_proyecto/FinanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.finance_app_proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
